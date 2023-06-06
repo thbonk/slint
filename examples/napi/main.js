2362fe016e3375dfb914b8b0fd00226b991c2cdc
Florian Blasius <florian.blasius@slint-ui.com>
2023-06-06 16:10:24 +0200

    Run window from napi

--- examples/napi/main.js
+++ examples/napi/main.js
@@ -1,18 +1,7 @@
 import { ComponentCompiler } from 'slint-ui';
 
-var componentCompiler = new ComponentCompiler();
-componentCompiler.setStyle("fluent");
-console.log(componentCompiler.style());
-
-// import * as fs from 'fs';
-// const fileName = "window.slint";
-// const fileData = fs.readFileSync(fileName, "utf8");
-
-// console.log(fileData);
-
-// // run(fileData).run();
-
-// var instance = new Instance(fileData);
-// instance.run();
-
-// // console.log(run(fileData))
+let compiler = new ComponentCompiler();
+let definition = compiler.buildFromPath("window.slint");
+console.log(definition.name);
+let instance = definition.create();
+instance.run();
